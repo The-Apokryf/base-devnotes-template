eef9d5b83c7b533d040b06411f957141e5d18b83
Sett Sarverott A.A.B <sarverott@gmail.com>
2025-12-17 10:16:40 +0100

    fixing prepublication routines  with now pointing UNIXUSAT1765963000750

--- toolscripts/prepublication-routines.sh
+++ toolscripts/prepublication-routines.sh
@@ -21,14 +21,4 @@ git commit -m "npm package refresh with fixes including locking procedure at pub
 #git add package.json package-lock.json npm-shrinkwrap.json 
 #git commit -m "production of publication setup files at publish of vesion {{.PROJECT_VERSION}} "
 #git push
-cp package-lock.json tmp.package-lock.json
-npm shrinkwrap
-node -e "fs.writeFileSync('./npm-shrinkwrap.smelted.json', JSON.stringify(JSON.parse(fs.readFileSync('./npm-shrinkwrap.json'))))  on now at $(node -e "console.log('UNIXUSAT'+Date.now())")"
-mv ./tmp.package-lock.json ./package-lock.json
-rm ./npm-shrinkwrap.json
-mv ./npm-shrinkwrap.smelted.json ./npm-shrinkwrap.json
-git add .
-git commit -m "production of publication setup files at publish of vesion new  on now at $(node -e "console.log('UNIXUSAT'+Date.now())")"
-
-
 
